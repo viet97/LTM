4ca15c601fbb2bbe084c77750158298ea8d46c9a
VietDQ <vietdq@talent.ominext.com>
2018-10-10 15:45:46 +0700

    finish login

--- app/src/main/java/com/example/macosx/ltm/activities/Home.java
+++ app/src/main/java/com/example/macosx/ltm/activities/Home.java
@@ -1,9 +1,11 @@
 package com.example.macosx.ltm.activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.macosx.ltm.R;
+import com.gigamole.navigationtabstrip.NavigationTabStrip;
 
 public class Home extends AppCompatActivity {
 
@@ -11,5 +13,24 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupUI();
+    }
+    private void setupUI(){
+        final NavigationTabStrip navigationTabStrip = (NavigationTabStrip) findViewById(R.id.nts);
+        navigationTabStrip.setTitles("Nav", "Tab", "Strip");
+        navigationTabStrip.setTabIndex(0, true);
+        navigationTabStrip.setTitleSize(25);
+        navigationTabStrip.setStripColor(Color.BLUE);
+        navigationTabStrip.setStripWeight(6);
+        navigationTabStrip.setStripFactor(2);
+        navigationTabStrip.setStripType(NavigationTabStrip.StripType.LINE);
+        navigationTabStrip.setStripGravity(NavigationTabStrip.StripGravity.BOTTOM);
+        navigationTabStrip.setTypeface("fonts/typeface.ttf");
+        navigationTabStrip.setCornersRadius(3);
+        navigationTabStrip.setAnimationDuration(300);
+        navigationTabStrip.setInactiveColor(Color.GRAY);
+        navigationTabStrip.setActiveColor(Color.WHITE);
+//        navigationTabStrip.setOnPageChangeListener(...);
+//        navigationTabStrip.setOnTabStripSelectedIndexListener(...);
     }
 }
